da34e763d811d155f4abd076dabcf486ef9d25a0
Miguel Castilho Dias <miguelcastilhodias@gmail.com>
2025-02-16 20:17:23 +0100

    Revert "Update Terraform cloudflare to v5"

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,7 +12,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "5.1.0"
+      version = "4.52.0"
     }
     random = {
       source  = "hashicorp/random"
