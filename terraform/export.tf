8021b69c1f928cd379f930ec7e2e41491ff1049e
Miguel Castilho <miguelcastilhodias@gmail.com>
2025-03-19 00:08:46 +0100

    improve security

--- terraform/export.tf
+++ terraform/export.tf
@@ -1,17 +1,57 @@
+resource "null_resource" "vault_encrypt_secrets" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo '${random_id.tunnel_secret.b64_std}' | ansible-vault encrypt_string --stdin-name 'cf_tunnel_secret' > ${path.module}/temp_tunnel_secret.txt
+      echo '${var.cloudflare_token}' | ansible-vault encrypt_string --stdin-name 'cf_token' > ${path.module}/temp_cf_token.txt
+      echo '${tailscale_tailnet_key.tailscale_key.key}' | ansible-vault encrypt_string --stdin-name 'tailscale_authkey' > ${path.module}/temp_tailscale_key.txt
+    EOT
+  }
+
+  triggers = {
+    tunnel_secret = random_id.tunnel_secret.b64_std
+    tailscale_key = tailscale_tailnet_key.tailscale_key.key
+  }
+}
+
+data "local_file" "encrypted_tunnel_secret" {
+  filename = "${path.module}/temp_tunnel_secret.txt"
+  depends_on = [null_resource.vault_encrypt_secrets]
+}
+
+data "local_file" "encrypted_cf_token" {
+  filename = "${path.module}/temp_cf_token.txt"
+  depends_on = [null_resource.vault_encrypt_secrets]
+}
+
+data "local_file" "encrypted_tailscale_key" {
+  filename = "${path.module}/temp_tailscale_key.txt"
+  depends_on = [null_resource.vault_encrypt_secrets]
+}
+
 resource "local_file" "tf_ansible_vars" {
   content = templatefile("${path.module}/templates/tf_ansible_vars.yml.tpl", {
     cf_tunnel_id        = cloudflare_zero_trust_tunnel_cloudflared.mediabox.id
     cf_account_id       = var.cloudflare_account_id
     cf_tunnel_name      = cloudflare_zero_trust_tunnel_cloudflared.mediabox.name
-    cf_tunnel_secret    = random_id.tunnel_secret.b64_std
-    cf_token            = var.cloudflare_token
-    tailscale_authkey   = tailscale_tailnet_key.tailscale_key.key
+    cf_tunnel_secret    = trimspace(regex("\\$ANSIBLE_VAULT.*", data.local_file.encrypted_tunnel_secret.content))
+    cf_token            = trimspace(regex("\\$ANSIBLE_VAULT.*", data.local_file.encrypted_cf_token.content))
+    tailscale_authkey   = trimspace(regex("\\$ANSIBLE_VAULT.*", data.local_file.encrypted_tailscale_key.content))
     mediabox_ip_address = var.mediabox_ip_address
   })
 
   filename = "../ansible/tf_ansible_vars.yml"
 
   depends_on = [
-    tailscale_tailnet_key.tailscale_key
+    tailscale_tailnet_key.tailscale_key,
+    cloudflare_zero_trust_tunnel_cloudflared.mediabox,
+    random_id.tunnel_secret,
+    null_resource.vault_encrypt_secrets,
+    data.local_file.encrypted_tunnel_secret,
+    data.local_file.encrypted_cf_token,
+    data.local_file.encrypted_tailscale_key
   ]
+
+  provisioner "local-exec" {
+    command = "rm -f ${path.module}/temp_*.txt"
+  }
 }
--- terraform/templates/tf_ansible_vars.yml.tpl
+++ terraform/templates/tf_ansible_vars.yml.tpl
@@ -1,9 +1,19 @@
 ---
 # Ansible vars_file containing variable values from Terraform.
+# Non-sensitive variables
 tf_cf_tunnel_id: ${cf_tunnel_id}
 tf_cf_account_id: ${cf_account_id}
 tf_cf_tunnel_name: ${cf_tunnel_name}
-tf_cf_tunnel_secret: ${cf_tunnel_secret}
-tf_cf_token: ${cf_token}
-tf_tailscale_authkey: ${tailscale_authkey}
 tf_mediabox_ip_address: ${mediabox_ip_address}
+
+# Sensitive variables - these will be encrypted by Ansible Vault
+# during the template rendering process
+tf_cf_tunnel_secret: !vault |
+          $ANSIBLE_VAULT;1.1;AES256
+          ${cf_tunnel_secret}
+tf_cf_token: !vault |
+          $ANSIBLE_VAULT;1.1;AES256
+          ${cf_token}
+tf_tailscale_authkey: !vault |
+          $ANSIBLE_VAULT;1.1;AES256
+          ${tailscale_authkey}
